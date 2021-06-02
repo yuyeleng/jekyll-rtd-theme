05f956786de23aa6cc64f6a2908e588eb2edf393
zhaozilong <1289586268@qq.com>
2021-06-02 15:16:04 +0800

    9

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,9 @@ $(function(){
     var itemH3 = $(".menu-uls>.toc").children('.itemHide').find('h3').text()
     console.log(itemH3)
 
-
+    //头部导航栏切换
+    $(".nav-box>.nav-item").click(function(){
+        $(this).addClass('active').siblings().removeClass('active')
+    })
 
 })
