f3b77411fbe268e3a53962fdfd2c4f098e7ea6e5
zhaozilong <1289586268@qq.com>
2021-06-03 14:21:30 +0800

    提交代码

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,10 +5,23 @@ $(function(){
     var locaHref = location.href
     var locaOrigin = location.origin
     var pageUrl = locaHref.replace(locaOrigin, '')
-    $(".header-box>.nav-box>.nav-item>a[href="+ pageUrl +"]").parents('.nav-item').addClass('active').siblings().removeClass('active')
+    $(".header-box>.nav-box>.nav-item>a[href='"+ pageUrl +"']").parents('.nav-item').addClass('active').siblings().removeClass('active')
     //头部导航栏切换
     $(".nav-box>.nav-item>a").click(function(){
         $(this).parent('.nav-item').addClass('active').siblings().removeClass('active')
     })
 
+
+    //设置语言
+    var lang = pageUrl.split('/')[1]
+    var langStr = '中文'
+    var newLang = 'zh'
+    if(lang === 'zh'){
+        langStr = 'English'
+        newLang = 'en'
+    }
+    $(".lang-box>.lang").text(langStr)
+    $(".lang-box>.lang").click(function(){
+        location.href = locaHref.replace(lang, newLang)
+    })
 })
