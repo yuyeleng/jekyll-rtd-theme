ff91d8e7470504382380e9a0d79dda641908c45e
zhaozilong <1289586268@qq.com>
2021-06-03 16:02:49 +0800

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@ $(function(){
     var modulePath = pageUrl.split('/')[2]
     console.log(modulePath)
     $(".menu-box .menu-uls:not(."+ modulePath +")").hide()
-    $(".header-box>.nav-box>.nav-item>a[data-menu='"+ modulePath +"']").parents('.nav-item').addClass('active').siblings().removeClass('active')
+    $(".header-box>.nav-box>.nav-item[data-menu='"+ modulePath +"']").addClass('active').siblings().removeClass('active')
 
 
     //设置语言
