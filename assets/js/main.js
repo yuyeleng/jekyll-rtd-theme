091cba287b0f7976dd4208afe80cb1b03557506d
zhaozilong <1289586268@qq.com>
2021-06-03 13:54:25 +0800

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ $(function(){
     var itemH3 = $(".menu-uls>.toc").children('.itemHide').find('h3').text()
     console.log(itemH3)
 
+    var locaHref = location.href
+    var locaOrigin = location.origin
+    var pageUrl = locaHref.replace(locaOrigin, '')
+    $(".header-box>.nav-box>.nav-item>a[href="+ pageUrl +"]").parents('.nav-item').addClass('active').siblings().removeClass('active')
     //头部导航栏切换
     $(".nav-box>.nav-item>a").click(function(){
         $(this).parent('.nav-item').addClass('active').siblings().removeClass('active')
