7592c0d00db4459ec69ec27f39d246c6890bc91c
zhaozilong <1289586268@qq.com>
2021-06-03 15:45:16 +0800

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,6 @@ $(function(){
     //模块路径
     var modulePath = pageUrl.split('/')[2]
     console.log(modulePath)
-    $(".menu-box .menu-uls:not(."+ modulePath +")").remove()//在同一页面下只显示同一模块下的菜单
 
 
     //设置语言
@@ -35,5 +34,5 @@ $(function(){
     })
     //隐藏非当前语言下的导航栏和目录
     $(".header-box>.nav-box>.nav-item:not(."+ lang +")").remove()
-    $(".menu-box .menu-uls:not(."+ lang +")").remove()
+    $(".menu-box .menu-uls:not(."+ lang +' '+ modulePath +")").remove()//在同一页面下只显示同一模块下的菜单
 })
